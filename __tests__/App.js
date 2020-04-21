f452d6bf45e2cfa76d8915da95e9865b4da82f2b
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-21 20:49:50 +0100

    social feed app

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,10 +1,34 @@
-import 'react-native';
 import React from 'react';
-import App from '../App.tsx';
+import SafeAreaView from 'react-native-safe-area-view';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StreamApp, FlatFeed, Activity, LikeButton, StatusUpdateForm } from 'expo-activity-feed';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+const CustomActivity = (props) => {
+  return (
+    <Activity
+    {...props}
+    Footer={
+      <LikeButton {...props} />
+    }
+   />
+ );
+};
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex: 1}} forceInset={{ top: 'always' }}>
+        <StreamApp
+            apiKey="5rqsbgqvqphs"
+            appId="40273"
+            token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNzg5MWZmM2ItOWJjZS00ODE1LWIwYzktMjA1NDI0NzkzYTMyIn0.4zAEraALwEHNXnyq36lqdAWyAIxlSAno1yapbPYopSM"
+        >
+        <FlatFeed Activity={CustomActivity} notify />
+        <StatusUpdateForm feedGroup="https://goo.gl/Hok8hp" />
+        </StreamApp>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
